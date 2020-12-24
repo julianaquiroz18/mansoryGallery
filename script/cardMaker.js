592321b4e5908324467759d298dbe6fae638b63d
Juliana Quiroz <jeffersonvelez@Jeffersons-MacBook-Pro.local>
2020-12-23 19:01:48 -0500

    CSS animations

--- script/cardMaker.js
+++ script/cardMaker.js
@@ -10,12 +10,12 @@ function makeImagesCards(images, htmlNode) {
     const imagesHTML = images.map((image) => {
         const imageURL = image.largeImageURL;
         const imageTitle = image.tags;
-        return cardMarkup(imageURL, imageTitle);
+        const imageId = image.id;
+        return cardMarkup(imageId, imageURL, imageTitle);
     });
     htmlNode.innerHTML = "";
     htmlNode.innerHTML += imagesHTML.join("\n");
-    htmlNode.querySelectorAll('.card').forEach((card) => card.addEventListener('mouseenter', animateImage));
-    htmlNode.querySelectorAll('.card').forEach((card) => card.addEventListener('mouseleave', hideTitle));
+    images.forEach(image => loadImage(image.largeImageURL, `.img-${image.id}`));
     htmlNode.querySelectorAll('.delete').forEach((button) => button.addEventListener('click', deleteImage));
 };
 
@@ -26,16 +26,34 @@ function makeImagesCards(images, htmlNode) {
  * @param {string} title
  * @returns {string}
  */
-const cardMarkup = ((url, title) => {
+const cardMarkup = ((id, url, title) => {
     return `    
     <div class="card position-relative">
-        <a href="${url}" target="blank"><img class="card-img-top image" src="${url}" alt="${title}"></a>
+        <a href="${url}" target="blank"><img class="img-${id} card-img-top image" src="./assets/loading.gif" alt="${title}"></a>
         <div class="d-flex p-2 justify-content-between align-items-baseline hidden card__header position-absolute">
             <h4 class="text-white m-0">${title}</h4>
             <button type="button" class="btn btn-sm btn-outline-light ml-1 delete"><i class="fa fa-trash" aria-hidden="true"></i></button>
         </div>
     </div>`
 });
+
+async function loadImage(imagePath, where) {
+    const target = document.querySelector(where);
+    try {
+        const response = await fetch(imagePath);
+        if (response.ok) {
+            target.style.filter = 'opacity(0)';
+            const miBlob = await response.blob()
+            const objectURL = URL.createObjectURL(miBlob);
+            target.onload = () => target.style.filter = '';
+            target.src = objectURL;
+        } else {
+            console.log('Respuesta de red OK pero respuesta HTTP no OK');
+        }
+    } catch (error) {
+        console.log('Hubo un problema con la petición Fetch:' + error.message);
+    }
+}
 
 
 export {
--- script/eventsCallbacks.js
+++ script/eventsCallbacks.js
@@ -1,15 +1,15 @@
 function animateImage(e) {
     const card = e.currentTarget;
     card.querySelector('.card__header').classList.add('appear');
-    card.style.zIndex = '100';
-    card.animate([
-        { transform: "scale(1)" },
-        { transform: "scale(1.25)" },
-        { transform: "scale(1)" },
-    ], {
-        duration: 700,
-        easing: "linear"
-    })
+    // card.style.zIndex = '100';
+    // card.animate([
+    //     { transform: "scale(1)" },
+    //     { transform: "scale(1.25)" },
+    //     // { transform: "scale(1)" },
+    // ], {
+    //     duration: 700,
+    //     easing: "linear"
+    // })
 }
 
 function hideTitle(e) {
